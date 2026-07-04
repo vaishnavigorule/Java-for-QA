a54056dcdbc3339a45d4735839d30204c0fe9361
Vaishnavi Sayaji Gorule <142771916+vaishnavigorule@users.noreply.github.com>
2026-07-04 15:51:46 +0530

    Add Variables.java with basic variable declarations

---
+++ 01-Basics/Variables.java
@@ -0,0 +1,15 @@
+public class variable{
+  public static void main (String[] args){
+    String name = "Vaishnavi";
+    int age = 21;
+    double salary = 40000;
+    char grade ='A';
+    boolean isQAEngineer = true;
+
+    System.out.println("Name:" + name);
+    System.out.println("Age:" + age);
+    System.out.println("Salary:" + salary);
+    System.out.println("Grade:" + grade);
+    System.out.println("QA Engineer:" + isQAEngineer);
+  }
+}
