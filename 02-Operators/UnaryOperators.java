8ad5d0709a0f86ff90ad10ae29cb2668487222e6
Vaishnavi Sayaji Gorule <142771916+vaishnavigorule@users.noreply.github.com>
2026-07-05 23:01:40 +0530

    Create UnaryOperators.java

---
+++ 02-Operators/UnaryOperators.java
@@ -0,0 +1,20 @@
+public class UnaryOperators {
+
+    public static void main(String[] args) {
+
+        int x = 5;
+
+        System.out.println(++x);
+
+        System.out.println(x++);
+
+        System.out.println(x);
+
+        System.out.println(--x);
+
+        System.out.println(x--);
+
+        System.out.println(x);
+
+    }
+}
