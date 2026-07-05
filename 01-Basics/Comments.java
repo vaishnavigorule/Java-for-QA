8a7de6d3ee32ec702620de2cca5893a81a796315
Vaishnavi Sayaji Gorule <142771916+vaishnavigorule@users.noreply.github.com>
2026-07-05 18:19:30 +0530

    Create Comments.java

---
+++ 01-Basics/Comments.java
@@ -0,0 +1,13 @@
+public class Comments {
+    public static void main(String[] args) {
+
+        // This is a single-line comment
+
+        /*
+         This is a
+         multi-line comment
+        */
+
+        System.out.println("Java Comments Example");
+    }
+}
