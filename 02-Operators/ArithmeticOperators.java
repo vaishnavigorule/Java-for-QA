49ae122b67245c08b5e61e618c25e484ed894f7a
Vaishnavi Sayaji Gorule <142771916+vaishnavigorule@users.noreply.github.com>
2026-07-05 22:59:39 +0530

    Create ArithmeticOperators.java

---
+++ 02-Operators/ArithmeticOperators.java
@@ -0,0 +1,14 @@
+public class ArithmeticOperators{
+  public static void main(String[] args){
+
+  int a = 13;
+  int b = 79;
+
+  System.out.println("Addition: " + (a + b));
+  System.out.println("Subtraction: " + (a - b));
+  System.out.println("Multiplication: " + (a * b));
+  System.out.println("Division: " + (a / b));
+  System.out.println("Modulus: " + (a % b));
+
+    }
+}
