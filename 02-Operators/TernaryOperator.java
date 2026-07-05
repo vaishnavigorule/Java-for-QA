8ec8f2ed5a1ab885cd28649b50138afc386cff05
Vaishnavi Sayaji Gorule <142771916+vaishnavigorule@users.noreply.github.com>
2026-07-05 23:02:11 +0530

    Add TernaryOperator class for voting eligibility check

---
+++ 02-Operators/TernaryOperator.java
@@ -0,0 +1,12 @@
+public class TernaryOperator {
+
+    public static void main(String[] args) {
+
+        int age = 20;
+
+        String result = (age >= 18) ? "Eligible to Vote" : "Not Eligible";
+
+        System.out.println(result);
+
+    }
+}
