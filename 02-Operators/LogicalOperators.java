9723d2d52016f7c0bc92b1e5b7453ebdb8e998d0
Vaishnavi Sayaji Gorule <142771916+vaishnavigorule@users.noreply.github.com>
2026-07-05 23:00:46 +0530

    Create LogicalOperators.java

---
+++ 02-Operators/LogicalOperators.java
@@ -0,0 +1,15 @@
+public class LogicalOperators {
+
+    public static void main(String[] args) {
+
+        boolean isLoggedIn = true;
+        boolean isAdmin = false;
+
+        System.out.println(isLoggedIn && isAdmin);
+
+        System.out.println(isLoggedIn || isAdmin);
+
+        System.out.println(!isLoggedIn);
+
+    }
+}
