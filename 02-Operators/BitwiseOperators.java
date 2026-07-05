e718eeebf8e4cdbd42752077bbec3505d1faeae5
Vaishnavi Sayaji Gorule <142771916+vaishnavigorule@users.noreply.github.com>
2026-07-05 23:03:42 +0530

    Create BitwiseOperators.java

---
+++ 02-Operators/BitwiseOperators.java
@@ -0,0 +1,15 @@
+public class BitwiseOperators {
+
+    public static void main(String[] args) {
+
+        int a = 5;
+        int b = 3;
+
+        System.out.println(a & b);
+
+        System.out.println(a | b);
+
+        System.out.println(a ^ b);
+
+    }
+}
