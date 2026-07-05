58427f4b4fdeeb1ab367b6aa0ef0efb07a54e79f
Vaishnavi Sayaji Gorule <142771916+vaishnavigorule@users.noreply.github.com>
2026-07-05 23:01:18 +0530

    Create AssignmentOperators.java

---
+++ 02-Operators/AssignmentOperators.java
@@ -0,0 +1,20 @@
+public class AssignmentOperators {
+
+    public static void main(String[] args) {
+
+        int x = 10;
+
+        x += 5;
+        System.out.println(x);
+
+        x -= 3;
+        System.out.println(x);
+
+        x *= 2;
+        System.out.println(x);
+
+        x /= 4;
+        System.out.println(x);
+
+    }
+}
