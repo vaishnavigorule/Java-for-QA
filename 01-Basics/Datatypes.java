2e50f8d19d57a4828498174d840e74fb54c674e5
Vaishnavi Sayaji Gorule <142771916+vaishnavigorule@users.noreply.github.com>
2026-07-05 18:15:23 +0530

    Create Datatypes.java

---
+++ 01-Basics/Datatypes.java
@@ -0,0 +1,22 @@
+public class datatype{
+  public static void main (String[] args ){
+
+    byte b=100;
+    sort s=1000;
+    int i=1;
+    long l=102000L;
+    float f=10.5f;
+    double d=20.99;
+    char c='a';
+    boolean b=true;
+
+    System.out.println("Byte: " + b);
+    System.out.println("Short: " + s);
+    System.out.println("Int: " + i);
+    System.out.println("Long: " + l);
+    System.out.println("Float: " + f);
+    System.out.println("Double: " + d);
+    System.out.println("Char: " + c);
+    System.out.println("Boolean: " + status);
+    }
+}
