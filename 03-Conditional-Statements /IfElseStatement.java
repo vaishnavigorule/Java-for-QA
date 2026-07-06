c24661a7dafe60d4e6bc443bed566627f4950fbe
Vaishnavi Sayaji Gorule <142771916+vaishnavigorule@users.noreply.github.com>
2026-07-06 12:04:13 +0530

    Create IfElseStatement.java

---
+++ 03-Conditional-Statements /IfElseStatement.java
@@ -0,0 +1,13 @@
+public class IfElseStatement {
+
+    public static void main(String[] args) {
+
+        int age = 16;
+
+        if (age >= 18) {
+            System.out.println("Eligible for vote");
+        } else{
+            System.out.println("Not Eligible for vote");
+        }
+    }
+}
