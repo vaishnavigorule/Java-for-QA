96b802bfd707eed83d12051a9531766b41e6bd47
Vaishnavi Sayaji Gorule <142771916+vaishnavigorule@users.noreply.github.com>
2026-07-06 12:01:50 +0530

    Create IfStatement.java

---
+++ 03-Conditional-Statements /IfStatement.java
@@ -0,0 +1,12 @@
+public class IfStatement {
+
+    public static void main(String[] args) {
+
+        int marks = 80;
+
+        if (marks >= 35) {
+            System.out.println("Pass");
+        }
+
+    }
+}
