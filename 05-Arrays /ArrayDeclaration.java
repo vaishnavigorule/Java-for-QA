afeb4161539454f182ad2f28151ef4b3e06e59ff
Vaishnavi Sayaji Gorule <142771916+vaishnavigorule@users.noreply.github.com>
2026-07-07 10:47:54 +0530

    Create ArrayDeclaration.java

---
+++ 05-Arrays /ArrayDeclaration.java
@@ -0,0 +1,10 @@
+public class ArrayDeclaration {
+
+    public static void main(String[] args) {
+
+        int[] numbers = new int[5];
+
+        System.out.println("Array created successfully.");
+
+    }
+}
