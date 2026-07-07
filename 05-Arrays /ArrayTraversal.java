d181196b6a995a7df4f4d28ec1fb6eb70d4e035d
Vaishnavi Sayaji Gorule <142771916+vaishnavigorule@users.noreply.github.com>
2026-07-07 10:48:58 +0530

    Create ArrayTraversal.java

---
+++ 05-Arrays /ArrayTraversal.java
@@ -0,0 +1,12 @@
+public class ArrayTraversal {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {5,10,15,20,25};
+
+        for(int i=0;i<numbers.length;i++){
+            System.out.println(numbers[i]);
+        }
+
+    }
+}
