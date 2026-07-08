4850fc52b063d882647c42617fb298952d598a57
Vaishnavi Sayaji Gorule <142771916+vaishnavigorule@users.noreply.github.com>
2026-07-08 11:33:07 +0530

    Create StringMethods.java

---
+++ 06-Strings/StringMethods.java
@@ -0,0 +1,14 @@
+public class StringMethods {
+
+    public static void main(String[] args) {
+
+        String text = "Manual Testing";
+
+        System.out.println(text.length());
+        System.out.println(text.toUpperCase());
+        System.out.println(text.toLowerCase());
+        System.out.println(text.charAt(0));
+        System.out.println(text.contains("Testing"));
+
+    }
+}
