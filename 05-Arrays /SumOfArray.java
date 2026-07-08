83d852f5f5f40b5101af25d8b716a4035738fc15
Vaishnavi Sayaji Gorule <142771916+vaishnavigorule@users.noreply.github.com>
2026-07-08 11:24:14 +0530

    Create SumOfArray.java

---
+++ 05-Arrays /SumOfArray.java
@@ -0,0 +1,16 @@
+public class SumOfArray {
+
+    public static void main(String[] args) {
+
+        int[] numbers={10,20,30,40};
+
+        int sum=0;
+
+        for(int num:numbers){
+            sum+=num;
+        }
+
+        System.out.println("Sum = "+sum);
+
+    }
+}
