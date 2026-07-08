c3a296d9d6111f3778413de6ea2c7807fb357abc
Vaishnavi Sayaji Gorule <142771916+vaishnavigorule@users.noreply.github.com>
2026-07-08 11:27:12 +0530

    Create ReverseArray.java

---
+++ 05-Arrays /ReverseArray.java
@@ -0,0 +1,12 @@
+public class ReverseArray {
+
+    public static void main(String[] args) {
+
+        int[] numbers={10,20,30,40,50};
+
+        for(int i=numbers.length-1;i>=0;i--){
+            System.out.print(numbers[i]+" ");
+        }
+
+    }
+}
