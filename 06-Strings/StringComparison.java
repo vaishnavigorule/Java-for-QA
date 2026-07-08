bf47c766825bcb2bba8e7de4705ef4c670764551
Vaishnavi Sayaji Gorule <142771916+vaishnavigorule@users.noreply.github.com>
2026-07-08 11:33:30 +0530

    Create StringComparison.java

---
+++ 06-Strings/StringComparison.java
@@ -0,0 +1,12 @@
+public class StringComparison {
+
+    public static void main(String[] args) {
+
+        String s1 = "Java";
+        String s2 = "Java";
+
+        System.out.println(s1.equals(s2));
+        System.out.println(s1.equalsIgnoreCase("JAVA"));
+
+    }
+}
