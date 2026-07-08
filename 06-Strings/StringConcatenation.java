67852a121c14b356d90c296a0634f3bb0ed11d46
Vaishnavi Sayaji Gorule <142771916+vaishnavigorule@users.noreply.github.com>
2026-07-08 11:33:55 +0530

    Create StringConcatenation.java

---
+++ 06-Strings/StringConcatenation.java
@@ -0,0 +1,11 @@
+public class StringConcatenation {
+
+    public static void main(String[] args) {
+
+        String first = "QA";
+        String second = "Engineer";
+
+        System.out.println(first + " " + second);
+
+    }
+}
