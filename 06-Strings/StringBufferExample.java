9ed970e3d754320021d7d0011f12189ca2d184ce
Vaishnavi Sayaji Gorule <142771916+vaishnavigorule@users.noreply.github.com>
2026-07-08 11:34:19 +0530

    Create StringBufferExample.java

---
+++ 06-Strings/StringBufferExample.java
@@ -0,0 +1,12 @@
+public class StringBufferExample {
+
+    public static void main(String[] args) {
+
+        StringBuffer sb = new StringBuffer("Java");
+
+        sb.append(" Programming");
+
+        System.out.println(sb);
+
+    }
+}
