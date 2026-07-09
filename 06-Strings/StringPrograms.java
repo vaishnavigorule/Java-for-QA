761906027b0510900a2f63e44cc32a1d7f9e6420
Vaishnavi Sayaji Gorule <142771916+vaishnavigorule@users.noreply.github.com>
2026-07-09 17:30:47 +0530

    Create StringPrograms.java

---
+++ 06-Strings/StringPrograms.java
@@ -0,0 +1,14 @@
+public class StringPrograms {
+
+    public static void main(String[] args) {
+
+        String text = "Software Testing";
+
+        System.out.println("Length = " + text.length());
+
+        System.out.println("Upper = " + text.toUpperCase());
+
+        System.out.println("Lower = " + text.toLowerCase());
+
+    }
+}
