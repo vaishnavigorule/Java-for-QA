03b724bf219cd818722c6df8b47a15fcc2fc2335
Vaishnavi Sayaji Gorule <142771916+vaishnavigorule@users.noreply.github.com>
2026-07-09 17:28:12 +0530

    Create CountWords.java

---
+++ 06-Strings/CountWords.java
@@ -0,0 +1,12 @@
+public class CountWords {
+
+    public static void main(String[] args) {
+
+        String sentence = "I Love Software Testing";
+
+        String[] words = sentence.split(" ");
+
+        System.out.println("Words = " + words.length);
+
+    }
+}
