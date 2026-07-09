5fa18d65fb2859e5ce4f8c080ecbf8960a42daba
Vaishnavi Sayaji Gorule <142771916+vaishnavigorule@users.noreply.github.com>
2026-07-09 17:26:13 +0530

    Create StringBuilderExample.java

---
+++ 06-Strings/StringBuilderExample.java
@@ -0,0 +1,12 @@
+public class StringBuilderExample {
+
+    public static void main(String[] args) {
+
+        StringBuilder sb = new StringBuilder("Hello");
+
+        sb.append(" QA");
+
+        System.out.println(sb);
+
+    }
+}
