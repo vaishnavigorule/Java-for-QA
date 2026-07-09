17183d08c2eb8a4fc69d42a985cf0b953a97200f
Vaishnavi Sayaji Gorule <142771916+vaishnavigorule@users.noreply.github.com>
2026-07-09 17:28:31 +0530

    Create RemoveSpaces.java

---
+++ 06-Strings/RemoveSpaces.java
@@ -0,0 +1,10 @@
+public class RemoveSpaces {
+
+    public static void main(String[] args) {
+
+        String text = "Software Testing";
+
+        System.out.println(text.replace(" ", ""));
+
+    }
+}
