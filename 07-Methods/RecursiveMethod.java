bb4c39361cb3d6c945c4cc1fd6e62829098c5680
Vaishnavi Sayaji Gorule <142771916+vaishnavigorule@users.noreply.github.com>
2026-07-11 16:44:55 +0530

    Create RecursiveMethod.java

---
+++ 07-Methods/RecursiveMethod.java
@@ -0,0 +1,17 @@
+public class RecursiveMethod {
+
+    static int factorial(int n) {
+
+        if(n == 1)
+            return 1;
+
+        return n * factorial(n - 1);
+
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(factorial(5));
+
+    }
+}
