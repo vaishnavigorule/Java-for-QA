b69f4701fc92cd9fe9d8060a65962a32b07bd013
Vaishnavi Sayaji Gorule <142771916+vaishnavigorule@users.noreply.github.com>
2026-07-11 16:43:00 +0530

    Add method to multiply two integers

---
+++ 07-Methods/MethodWithReturnType.java
@@ -0,0 +1,13 @@
+public class MethodWithReturnType {
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static void main(String[] args) {
+
+        int result = multiply(5, 6);
+
+        System.out.println("Result = " + result);
+    }
+}
