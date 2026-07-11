5220aa9fafcca51abafc7a307d021170f098bf62
Vaishnavi Sayaji Gorule <142771916+vaishnavigorule@users.noreply.github.com>
2026-07-11 16:43:23 +0530

    Create MethodWithoutReturnType.java

---
+++ 07-Methods/MethodWithoutReturnType.java
@@ -0,0 +1,10 @@
+public class MethodWithoutReturnType {
+
+    public static void greet() {
+        System.out.println("Hello QA Engineer!");
+    }
+
+    public static void main(String[] args) {
+        greet();
+    }
+}
