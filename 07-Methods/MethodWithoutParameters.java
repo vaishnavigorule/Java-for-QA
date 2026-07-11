556bee4a1fed42d48bc313c67e0c64f60d52aaba
Vaishnavi Sayaji Gorule <142771916+vaishnavigorule@users.noreply.github.com>
2026-07-11 16:42:08 +0530

    Create MethodWithoutParameters.java

---
+++ 07-Methods/MethodWithoutParameters.java
@@ -0,0 +1,10 @@
+public class MethodWithoutParameters {
+
+    public static void display() {
+        System.out.println("Welcome to Java Methods");
+    }
+
+    public static void main(String[] args) {
+        display();
+    }
+}
