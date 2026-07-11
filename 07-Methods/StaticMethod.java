8680d8e8faff3d46270c4cde07deea1f68ab56c1
Vaishnavi Sayaji Gorule <142771916+vaishnavigorule@users.noreply.github.com>
2026-07-11 16:43:46 +0530

    Create StaticMethod.java

---
+++ 07-Methods/StaticMethod.java
@@ -0,0 +1,10 @@
+public class StaticMethod {
+
+    static void message() {
+        System.out.println("Static Method");
+    }
+
+    public static void main(String[] args) {
+        message();
+    }
+}
