4889a5d6c3497a0167ea7c7cb8f2577bc840be52
Vaishnavi Sayaji Gorule <142771916+vaishnavigorule@users.noreply.github.com>
2026-07-12 10:15:38 +0530

    Create MethodCallingUsingObject.java

---
+++ 08-OOP/MethodCallingUsingObject.java
@@ -0,0 +1,17 @@
+public class MethodCallingUsingObject {
+
+    void greet() {
+
+        System.out.println("Welcome QA Engineer!");
+
+    }
+
+    public static void main(String[] args) {
+
+        MethodCallingUsingObject obj = new MethodCallingUsingObject();
+
+        obj.greet();
+
+    }
+
+}
