c76de2a75d9d6a97e59112846e4b8f0f4983189c
Vaishnavi Sayaji Gorule <142771916+vaishnavigorule@users.noreply.github.com>
2026-07-13 20:56:57 +0530

    Create DefaultConstructor.java

---
+++ 09-Constructor /DefaultConstructor.java
@@ -0,0 +1,13 @@
+public class DefaultConstructor {
+
+    DefaultConstructor() {
+        System.out.println("Default Constructor Called");
+    }
+
+    public static void main(String[] args) {
+
+        DefaultConstructor obj = new DefaultConstructor();
+
+    }
+
+}
