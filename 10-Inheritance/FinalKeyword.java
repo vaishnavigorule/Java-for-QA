07ab708607b82424be48158eef022a8af01636bb
Vaishnavi Sayaji Gorule <142771916+vaishnavigorule@users.noreply.github.com>
2026-07-18 18:10:39 +0530

    Create FinalKeyword.java

---
+++ 10-Inheritance/FinalKeyword.java
@@ -0,0 +1,19 @@
+class Vehicle {
+
+    final void speed() {
+        System.out.println("Maximum Speed = 120");
+    }
+
+}
+
+public class FinalKeyword {
+
+    public static void main(String[] args) {
+
+        Vehicle obj = new Vehicle();
+
+        obj.speed();
+
+    }
+
+}
