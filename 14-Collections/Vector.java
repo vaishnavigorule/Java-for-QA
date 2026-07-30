652e26a08a473da12cd224ad0b7e2b36379d086d
Vaishnavi Sayaji Gorule <142771916+vaishnavigorule@users.noreply.github.com>
2026-07-30 15:57:01 +0530

    Create Vector.java

---
+++ 14-Collections/Vector.java
@@ -0,0 +1,16 @@
+import java.util.Vector;
+
+public class VectorExample {
+
+    public static void main(String[] args) {
+
+        Vector<String> tools = new Vector<>();
+
+        tools.add("Selenium");
+        tools.add("JIRA");
+        tools.add("Postman");
+
+        System.out.println(tools);
+
+    }
+}
