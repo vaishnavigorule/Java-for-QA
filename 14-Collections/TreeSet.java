2c5bc8ea63dca1187bfe78a5ce5ead7f66c0f0f1
Vaishnavi Sayaji Gorule <142771916+vaishnavigorule@users.noreply.github.com>
2026-08-02 23:06:05 +0530

    Create TreeSet.java

---
+++ 14-Collections/TreeSet.java
@@ -0,0 +1,17 @@
+import java.util.TreeSet;
+
+public class TreeSetExample {
+
+    public static void main(String[] args) {
+
+        TreeSet<Integer> testCases = new TreeSet<>();
+
+        testCases.add(105);
+        testCases.add(101);
+        testCases.add(103);
+        testCases.add(102);
+
+        System.out.println(testCases);
+
+    }
+}
